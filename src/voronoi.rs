f35e37e48449e8dc2ef13e78a982d75dcc8fb333
Sameer Puri <sameer@users.noreply.github.com>
2021-05-30 15:50:04 -0400

    reduce voronoi runtime by considering cache locality

--- src/voronoi.rs
+++ src/voronoi.rs
@@ -18,32 +18,32 @@ pub fn compute_voronoi(points: &[[i64; 2]], width: usize, height: usize) -> Vec<
     // Claim: without a scratchpad, converge to the correct point assignment faster
     // let mut grid_scratchpad = grid.clone();
     let mut round_step = (width.max(height)).next_power_of_two() / 2;
+    let width = width as i64;
+    let height = height as i64;
     while round_step != 0 {
-        for i in 0..width as i64 {
-            for j in 0..height as i64 {
-                let mut votes = [[usize::MAX; 3]; 3];
-                const DIRECTIONS: [i64; 3] = [-1, 0, 1];
-                for x_dir in &DIRECTIONS {
-                    let x = i + x_dir * round_step as i64;
-                    if x < 0 || x >= width as i64 {
+        for j in 0..height {
+            for i in 0..width {
+                for y_dir in -1..=1 {
+                    let y = j + y_dir * round_step as i64;
+                    if y < 0 || y >= height {
                         continue;
                     }
-                    for y_dir in &DIRECTIONS {
-                        let y = j + y_dir * round_step as i64;
-                        if y < 0 || y >= height as i64 {
+                    for x_dir in -1..=1 {
+                        let x = i + x_dir * round_step as i64;
+                        if x < 0 || x >= width {
                             continue;
                         }
-                        votes[(x_dir + 1) as usize][(y_dir + 1) as usize] =
-                            grid[y as usize][x as usize];
+                        let new = grid[y as usize][x as usize];
+                        if new != usize::MAX {
+                            let current = grid[j as usize][i as usize];
+                            if current == usize::MAX
+                                || abs_distance_squared([i, j], points[new])
+                                    < abs_distance_squared([i, j], points[current])
+                            {
+                                grid[j as usize][i as usize] = new;
+                            }
+                        }
                     }
-                }
-                if let Some(new_color) = votes
-                    .iter()
-                    .flatten()
-                    .filter(|color| **color != usize::MAX)
-                    .min_by_key(|color| abs_distance_squared([i, j], points[**color]))
-                {
-                    grid[j as usize][i as usize] = *new_color;
                 }
             }
         }
