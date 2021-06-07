0925abed906ee349c174f4152ddbcf9eae6482d6
Sameer Puri <sameer@users.noreply.github.com>
2021-06-07 18:19:24 -0400

    add low branch count JFA

--- src/voronoi.rs
+++ src/voronoi.rs
@@ -1,55 +1,57 @@
 use crate::abs_distance_squared;
 
-/// Given a set of points in a bounding box from (0, 0) to (width, height),
+/// Given a set of sites in a bounding box from (0, 0) to (width, height),
 /// return the assignment of coordinates in that box to their nearest neighbor
 /// using the Jump Flooding Algorithm.
 ///
 /// https://www.comp.nus.edu.sg/~tants/jfa/i3d06.pdf
-pub fn compute_voronoi(points: &[[usize; 2]], width: usize, height: usize) -> Vec<Vec<usize>> {
-    if points.is_empty() {
+pub fn jump_flooding_voronoi(sites: &[[usize; 2]], width: usize, height: usize) -> Vec<Vec<usize>> {
+    if sites.is_empty() {
         return vec![];
     }
     // use usize::MAX to represent colorless cells
     let mut grid = vec![vec![usize::MAX; width]; height];
-    points.iter().enumerate().for_each(|(color, point)| {
-        grid[point[1] as usize][point[0] as usize] = color;
+    sites.iter().enumerate().for_each(|(color, site)| {
+        grid[site[1] as usize][site[0] as usize] = color;
     });
 
-    // Claim: don't actually need a scratchpad, without one we converge to the correct assignment faster
-    // let mut grid_scratchpad = grid.clone();
-    let mut round_step = (width.max(height)).next_power_of_two() / 2;
-
+    let mut round_step = (width.max(height))
+        .checked_next_power_of_two()
+        .map(|x| x / 2)
+        .unwrap_or_else(|| (width.max(height) / 2).next_power_of_two());
     while round_step != 0 {
-        for j in 0..height {
-            for i in 0..width {
-                for y_dir in -1..=1 {
-                    let y = match y_dir {
-                        -1 => j.checked_sub(round_step),
-                        0 => Some(j),
-                        1 => j.checked_add(round_step),
-                        _ => unreachable!(),
-                    }
-                    .unwrap_or(height);
-                    if y >= height {
-                        continue;
-                    }
-                    for x_dir in -1..=1 {
-                        let x = match x_dir {
-                            -1 => i.checked_sub(round_step),
-                            0 => Some(i),
-                            1 => i.checked_add(round_step),
+        for y_dir in -1..=1 {
+            let y_range = if y_dir == -1 { round_step } else { 0 }..if y_dir == 1 {
+                height.saturating_sub(round_step)
+            } else {
+                height
+            };
+            for x_dir in -1..=1 {
+                let x_range = if x_dir == -1 { round_step } else { 0 }..if x_dir == 1 {
+                    width.saturating_sub(round_step)
+                } else {
+                    width
+                };
+                for j in y_range.clone() {
+                    for i in x_range.clone() {
+                        let y = match y_dir {
+                            -1 => j - round_step,
+                            0 => j,
+                            1 => j + round_step,
                             _ => unreachable!(),
-                        }
-                        .unwrap_or(width);
-                        if x >= width {
-                            continue;
-                        }
+                        };
+                        let x = match x_dir {
+                            -1 => i - 1,
+                            0 => i,
+                            1 => i + round_step,
+                            _ => unreachable!(),
+                        };
                         let new = grid[y][x];
                         if new != usize::MAX {
                             let current = grid[j][i];
                             if current == usize::MAX
-                                || abs_distance_squared([i, j], points[new])
-                                    < abs_distance_squared([i, j], points[current])
+                                || abs_distance_squared([i, j], sites[new])
+                                    < abs_distance_squared([i, j], sites[current])
                             {
                                 grid[j][i] = new;
                             }
@@ -66,23 +68,34 @@ pub fn compute_voronoi(points: &[[usize; 2]], width: usize, height: usize) -> Ve
 }
 
 #[cfg(test)]
-#[test]
-fn test_voronoi() {
-    use image::ImageBuffer;
+mod tests {
+    use super::jump_flooding_voronoi;
+    use crate::abs_distance_squared;
 
-    let points = [[0, 0], [63, 63], [24, 5], [0, 127], [127, 127], [127, 240]];
-    const WIDTH: usize = 256;
-    const HEIGHT: usize = 256;
-    let grid = compute_voronoi(&points, WIDTH, HEIGHT);
-    let out = ImageBuffer::from_fn(WIDTH as u32, HEIGHT as u32, |x, y| {
-        let color = grid[y as usize][x as usize];
-        let r = (255. * (color as f64 / (points.len() + 3) as f64)).round() as u8;
-        if points.iter().any(|p| x == p[0] as u32 && y == p[1] as u32) {
-            image::Rgb([255, 0, 255])
-        } else {
-            image::Rgb([r, r, r])
+    #[test]
+    fn test_jump_flooding_voronoi() {
+        const WIDTH: usize = 256;
+        const HEIGHT: usize = 256;
+        let sites = [
+            [0, 0],
+            [0, HEIGHT - 1],
+            [WIDTH - 1, 0],
+            [WIDTH - 1, HEIGHT - 1],
+            [WIDTH / 2, HEIGHT / 2],
+        ];
+        let assignments = jump_flooding_voronoi(&sites, WIDTH, HEIGHT);
+        for j in 0..HEIGHT {
+            for i in 0..WIDTH {
+                let min_distance = sites
+                    .iter()
+                    .map(|site| abs_distance_squared(*site, [i, j]))
+                    .min()
+                    .unwrap();
+                let actual_distance = abs_distance_squared(sites[assignments[j][i]], [i, j]);
+
+                // Don't check the assigned site because of distance ties
+                assert_eq!(min_distance, actual_distance);
+            }
         }
-    });
-    out.save_with_format("out.png", image::ImageFormat::Png)
-        .unwrap();
+    }
 }
