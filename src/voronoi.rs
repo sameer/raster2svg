c989149353de402c23b4818d918c3d10b450cee0
Sameer Puri <sameer@users.noreply.github.com>
2021-06-21 18:52:32 -0400

    parallelize JFA with rayon

--- src/voronoi.rs
+++ src/voronoi.rs
@@ -2,7 +2,7 @@ use std::fmt::Debug;
 
 use log::warn;
 use lyon_geom::{point, vector, Angle, Line, LineSegment, Point, Vector};
-use ndarray::ArrayView2;
+use ndarray::{par_azip, prelude::*};
 use num_traits::{FromPrimitive, PrimInt, Signed};
 
 use crate::abs_distance_squared;
@@ -19,7 +19,7 @@ where
     fn dist(&self, point: [T; 2]) -> f64 {
         abs_distance_squared(*self, point).to_f64().unwrap()
     }
-    fn seeds(&self, width: usize, height: usize) -> Vec<[T; 2]> {
+    fn seeds(&self, _width: usize, _height: usize) -> Vec<[T; 2]> {
         vec![*self]
     }
 }
@@ -76,65 +76,67 @@ where
 /// using the Jump Flooding Algorithm.
 ///
 /// https://www.comp.nus.edu.sg/~tants/jfa/i3d06.pdf
-pub fn jump_flooding_voronoi<S: Site<T>, T: PrimInt + FromPrimitive + Debug>(
+pub fn jump_flooding_voronoi<S: Site<T> + Send + Sync, T: PrimInt + FromPrimitive + Debug>(
     sites: &[S],
     width: usize,
     height: usize,
-) -> Vec<Vec<usize>> {
-    if sites.is_empty() {
-        return vec![];
-    }
+) -> Array2<usize> {
     // use usize::MAX to represent colorless cells
-    let mut grid = vec![vec![usize::MAX; width]; height];
+    let mut grid = Array2::from_elem((width, height), usize::MAX);
+
+    if sites.is_empty() {
+        return grid;
+    }
     sites.iter().enumerate().for_each(|(color, site)| {
         for seed in site.seeds(width, height) {
-            grid[seed[1].to_usize().unwrap()][seed[0].to_usize().unwrap()] = color;
+            grid[[seed[0].to_usize().unwrap(), seed[1].to_usize().unwrap()]] = color;
         }
     });
+
+    let positions = Array::from_iter(
+        (0..width)
+            .map(|x| (0..height).map(move |y| [x, y]))
+            .flatten(),
+    )
+    .into_shape((width, height))
+    .unwrap();
+
+    let mut scratchpad = grid.clone();
 
     let mut round_step = (width.max(height))
         .checked_next_power_of_two()
         .map(|x| x / 2)
         .unwrap_or_else(|| (width.max(height) / 2).next_power_of_two());
+
+    let slice_info_fn = |x_dir, y_dir, step: isize| match (x_dir, y_dir) {
+        (-1, -1) => (s![step.., step..]),
+        (0, -1) => s![.., step..],
+        (-1, 0) => s![step.., ..],
+        (0, 0) => s![.., ..],
+        (1, 0) => s![..-step, ..],
+        (0, 1) => s![.., ..-step],
+        (-1, 1) => s![step.., ..-step],
+        (1, -1) => s![..-step, step..],
+        (1, 1) => s![..-step, ..-step],
+        _ => unreachable!(),
+    };
+
     while round_step != 0 {
         for y_dir in -1..=1 {
-            let y_range = if y_dir == -1 { round_step } else { 0 }..if y_dir == 1 {
-                height.saturating_sub(round_step)
-            } else {
-                height
-            };
             for x_dir in -1..=1 {
-                let x_range = if x_dir == -1 { round_step } else { 0 }..if x_dir == 1 {
-                    width.saturating_sub(round_step)
-                } else {
-                    width
-                };
-                for j in y_range.clone() {
-                    let y = match y_dir {
-                        -1 => j - round_step,
-                        0 => j,
-                        1 => j + round_step,
-                        _ => unreachable!(),
-                    };
-                    for i in x_range.clone() {
-                        let x = match x_dir {
-                            -1 => i - 1,
-                            0 => i,
-                            1 => i + round_step,
-                            _ => unreachable!(),
-                        };
-                        let new = grid[y][x];
-                        if new != usize::MAX {
-                            let current = grid[j][i];
-                            let here = [T::from_usize(i).unwrap(), T::from_usize(j).unwrap()];
-                            if current == usize::MAX
-                                || sites[new].dist(here) < sites[current].dist(here)
-                            {
-                                grid[j][i] = new;
+                let center_slice_info = slice_info_fn(x_dir, y_dir, round_step as isize);
+                let kernel_slice_info = slice_info_fn(-x_dir, -y_dir, round_step as isize);
+                par_azip! {
+                    (dest in scratchpad.slice_mut(center_slice_info), sample in grid.slice(kernel_slice_info), here in positions.slice(center_slice_info)) {
+                        let here = [T::from_usize(here[0]).unwrap(), T::from_usize(here[1]).unwrap()];
+                        if *sample != usize::MAX {
+                            if *dest == usize::MAX || sites[*sample].dist(here) < sites[*dest].dist(here) {
+                                *dest = *sample;
                             }
                         }
                     }
-                }
+                };
+                grid.assign(&scratchpad);
             }
         }
 
@@ -146,18 +148,17 @@ pub fn jump_flooding_voronoi<S: Site<T>, T: PrimInt + FromPrimitive + Debug>(
 
 pub fn colors_to_assignments<S: Site<T>, T: PrimInt + FromPrimitive + Debug>(
     sites: &[S],
-    grid: &[Vec<usize>],
+    grid: ArrayView2<usize>,
 ) -> Vec<Vec<[T; 2]>> {
-    let expected_assignment_capacity =
-        grid.len() * grid.first().map(|first| first.len()).unwrap_or(0) / sites.len();
+    if sites.is_empty() {
+        return vec![];
+    }
+    let expected_assignment_capacity = grid.shape()[0] * grid.shape()[1] / sites.len();
     let mut sites_to_points =
         vec![Vec::<[T; 2]>::with_capacity(expected_assignment_capacity); sites.len()];
-    for j in 0..grid.len() {
-        for i in 0..grid[j].len() {
-            sites_to_points[grid[j][i]]
-                .push([T::from_usize(i).unwrap(), T::from_usize(j).unwrap()]);
-        }
-    }
+    grid.indexed_iter().for_each(|((i, j), site)| {
+        sites_to_points[*site].push([T::from_usize(i).unwrap(), T::from_usize(j).unwrap()])
+    });
     sites_to_points
 }
 
@@ -241,11 +242,14 @@ pub fn calculate_cell_properties<T: PrimInt + Debug + Default>(
                         from: centroid,
                         to: intersection,
                     })
+                    .filter(|segment| segment.from != segment.to)
                 })
                 .collect::<Vec<_>>();
 
-            if edge_vectors.len() != 2 {
-                warn!("It should be impossible for this line to intersect the hull at more than two edges: {:?} {:?} {:?}", &hull, centroid, phi_vector);
+            if edge_vectors.len() > 2 {
+                unreachable!("The hull algorithm is wrong should this ever happen")
+            } else if edge_vectors.len() < 2 {
+                warn!("It should be impossible for this line to intersect the hull at less than two edges, but points may be collinear: {:?} {:?} {:?}", points, centroid, phi_vector);
                 let radius = (points.len() as f64 / std::f64::consts::PI).sqrt();
                 cell_properties.phi_oriented_segment_through_centroid = Some(LineSegment {
                     from: centroid + phi_vector * radius,
@@ -301,7 +305,7 @@ mod tests {
                     .min()
                     .unwrap();
                 let actual_distance =
-                    abs_distance_squared(sites[assignments[j][i]], [i as i64, j as i64]);
+                    abs_distance_squared(sites[assignments[[i, j]]], [i as i64, j as i64]);
 
                 // Don't check the assigned site because of distance ties
                 assert_eq!(min_distance, actual_distance);
